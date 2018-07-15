dafe4150d93acfc29cef3178ffc342cd34fb1db6
jdc20181 <jdc20181@gmail.com>
2018-07-15 12:15:46 -0400

    Add files via upload

---
+++ WindowsApp59/WindowsApp59/Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripDropDownButton1 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.CaptureToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripButton3 = New System.Windows.Forms.ToolStripButton()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.CopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ToolStrip1.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1, Me.ToolStripButton2, Me.ToolStripDropDownButton1, Me.ToolStripButton3})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(1114, 25)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.AutoToolTip = False
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(53, 22)
+        Me.ToolStripButton1.Text = "Capture"
+        '
+        'ToolStripButton2
+        '
+        Me.ToolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton2.Image = CType(resources.GetObject("ToolStripButton2.Image"), System.Drawing.Image)
+        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton2.Name = "ToolStripButton2"
+        Me.ToolStripButton2.Size = New System.Drawing.Size(35, 22)
+        Me.ToolStripButton2.Text = "Save"
+        '
+        'ToolStripDropDownButton1
+        '
+        Me.ToolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripDropDownButton1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CaptureToolStripMenuItem, Me.SaveToolStripMenuItem})
+        Me.ToolStripDropDownButton1.Image = CType(resources.GetObject("ToolStripDropDownButton1.Image"), System.Drawing.Image)
+        Me.ToolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripDropDownButton1.Name = "ToolStripDropDownButton1"
+        Me.ToolStripDropDownButton1.Size = New System.Drawing.Size(29, 22)
+        Me.ToolStripDropDownButton1.Text = "ToolStripDropDownButton1"
+        '
+        'CaptureToolStripMenuItem
+        '
+        Me.CaptureToolStripMenuItem.Name = "CaptureToolStripMenuItem"
+        Me.CaptureToolStripMenuItem.ShortcutKeys = CType(((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Shift) _
+            Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.CaptureToolStripMenuItem.Size = New System.Drawing.Size(190, 22)
+        Me.CaptureToolStripMenuItem.Text = "Capture"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.ShortcutKeys = CType(((System.Windows.Forms.Keys.Alt Or System.Windows.Forms.Keys.Shift) _
+            Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(190, 22)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'ToolStripButton3
+        '
+        Me.ToolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton3.Image = CType(resources.GetObject("ToolStripButton3.Image"), System.Drawing.Image)
+        Me.ToolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton3.Name = "ToolStripButton3"
+        Me.ToolStripButton3.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton3.Text = "ToolStripButton3"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 25)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1114, 575)
+        Me.TabControl1.TabIndex = 2
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.PictureBox1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(1106, 549)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "BeffsEasyCapture"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Gray
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1100, 543)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CopyToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(103, 26)
+        '
+        'CopyToolStripMenuItem
+        '
+        Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(102, 22)
+        Me.CopyToolStripMenuItem.Text = "&Copy"
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.TextBox1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(1106, 549)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(469, 177)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1114, 600)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "Form1"
+        Me.Text = "LightCapture"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents ToolStripButton2 As ToolStripButton
+    Friend WithEvents ToolStripDropDownButton1 As ToolStripDropDownButton
+    Friend WithEvents CaptureToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripButton3 As ToolStripButton
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ WindowsApp59/WindowsApp59/Form1.vb
@@ -0,0 +1,125 @@
+﻿Imports System
+Imports System.IO
+Imports System.Net
+Imports System.Text
+Public Class Form1
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click, CaptureToolStripMenuItem.Click
+        Me.Opacity = 0
+        Dim area As Rectangle
+        Dim capture As System.Drawing.Bitmap
+        Dim graph As Graphics
+        area = Screen.PrimaryScreen.WorkingArea
+        capture = New System.Drawing.Bitmap(area.Width, area.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb)
+        graph = Graphics.FromImage(capture)
+        graph.CopyFromScreen(area.X, area.Y, 0, 0, area.Size, CopyPixelOperation.SourceCopy)
+        PictureBox1.Image = capture
+        information()
+        AutoSave()
+
+    End Sub
+    Public Sub information()
+        Me.Opacity = 1
+
+
+    End Sub
+    Private Sub ToolStripButton2_Click(sender As Object, e As EventArgs) Handles ToolStripButton2.Click, SaveToolStripMenuItem.Click
+        Dim save As New SaveFileDialog
+        Try
+            save.Title = "Save File"
+            save.FileName = "Screenshot"
+            '  save.Filter = "png|*.png"
+            save.Filter = "Bitmap Image (.bmp)|*.bmp|JPEG Image (.jpeg)|*.jpeg|Png Image (.png)|*.png|Tiff Image (.tiff)|*.tiff"
+            If save.ShowDialog = Windows.Forms.DialogResult.OK Then
+                PictureBox1.Image.Save(save.FileName, Drawing.Imaging.ImageFormat.Png)
+            End If
+
+        Catch ex As Exception
+
+        End Try
+
+
+
+
+    End Sub
+    Public Sub AutoSave()
+        'Dim SavePath As String = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) & "ScreenShot-" & Now.ToString("ddd_dd_MM_yyyy_hh_mm_ss")
+
+        Dim strfilename = My.Computer.FileSystem.SpecialDirectories.MyPictures & "/BeffsEasyCapture/" & "ScreenShot-" & Now.ToString("ddd_dd_MM_yyyy_hh_mm_ss") & ".png"
+        'PictureBox1.Image.Save(SavePath, Drawing.Imaging.ImageFormat.Png)
+
+        ' Me.PictureBox1.Image.Save(IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.MyPictures & "/BeffsEasyCapture/", "ScreenShot-" & Now.ToString("ddd_dd_MM_yyyy_hh_mm_ss" & ".Png")))
+        Me.PictureBox1.Image.Save(IO.Path.Combine(strfilename))
+        Me.Text = "AutoSaved To: " & strfilename
+        PostToImgur(strfilename, "1d1225671adec37")
+
+    End Sub
+
+
+
+
+    '
+#Region "Others"
+
+#End Region
+
+
+
+
+
+
+    Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
+        My.Computer.Clipboard.Clear()
+
+        My.Computer.Clipboard.SetImage(PictureBox1.Image)
+
+
+    End Sub
+
+
+    Private Sub beffseasycapture_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ToolStripDropDownButton1.Visible = False
+    End Sub
+
+    Private Sub ToolStripButton3_Click(sender As Object, e As EventArgs) Handles ToolStripButton3.Click
+        ' EasyCapSettings.Show()
+
+
+    End Sub
+    Private Shared Sub Main(ByVal args As String())
+        '  PostToImgur(PictureBox1, IMGUR_ANONYMOUS_API_KEY)
+    End Sub
+
+    Public Shared Function PostToImgur(ByVal imagFilePath As String, ByVal apiKey As String) As String
+        Dim imageData As Byte()
+        Dim fileStream As FileStream = File.OpenRead(imagFilePath)
+        imageData = New Byte(fileStream.Length - 1) {}
+        fileStream.Read(imageData, 0, imageData.Length)
+        fileStream.Close()
+        Const MAX_URI_LENGTH As Integer = 32766
+        Dim base64img As String = System.Convert.ToBase64String(imageData)
+        Dim sb As StringBuilder = New StringBuilder()
+        Dim i As Integer = 0
+
+        While i < base64img.Length
+            sb.Append(Uri.EscapeDataString(base64img.Substring(i, Math.Min(MAX_URI_LENGTH, base64img.Length - i))))
+            i += MAX_URI_LENGTH
+        End While
+
+        Dim uploadRequestString As String = "key=" & apiKey & "&title=" & "imageTitle" & "&caption=" & "img" & "&image=" & sb.ToString()
+        Dim webRequest As HttpWebRequest = CType(webRequest.Create("https://api.imgur.com/3/image"), HttpWebRequest)
+        webRequest.Method = "POST"
+        webRequest.ContentType = "application/x-www-form-urlencoded"
+        webRequest.ServicePoint.Expect100Continue = False
+        webRequest.Headers("Authorization") = "Client-ID 1d1225671adec37"
+        Dim streamWriter As StreamWriter = New StreamWriter(webRequest.GetRequestStream())
+        streamWriter.Write(uploadRequestString)
+        streamWriter.Close()
+        Dim response As WebResponse = webRequest.GetResponse()
+        Dim responseStream As Stream = response.GetResponseStream()
+        Dim responseReader As StreamReader = New StreamReader(responseStream)
+        Return responseReader.ReadToEnd()
+
+        My.Computer.Clipboard.SetText(responseReader.ReadToEnd)
+        TextBox1.Text = responseReader.ReadToEnd
+    End Function
+End Class
